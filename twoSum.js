a71d34accda6a63256f8bc2fa8ed1f1bae9d88d1
Manami Ueda <manamiueda1@gmail.com>
2021-04-01 11:26:54 -0400

    added brute force towSum solution

--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,21 @@
 // twoSum Solution LeetCode
 // https://leetcode.com/problems/two-sum/
 
+// brute force O(N)^2
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+      for (let i = 0; i < array.length; i++){
+          for (let j = i+1; j < array.length; j++){
+              if (array[i] + array[j] === targetSum){
+                  return [array[i], array[j]]
+              }
+          }
+      }
+   return []
+  }
 
+
+  
 
 
 // twoNumberSum Solution AlgoExpert
